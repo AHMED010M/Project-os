f0aeef07c3d84e63e2da535aba9ee0a0e5abbb58
Abo Salah <91723027+AHMED010M@users.noreply.github.com>
2025-12-18 14:28:12 +0200

    Fix SocketClient.cpp - use username field instead of user

--- client_gui/SocketClient.cpp
+++ client_gui/SocketClient.cpp
@@ -1,8 +1,3 @@
-/*
- * MIT License
- * Copyright (c) 2025 OS Chat Project
- */
-
 #include "SocketClient.h"
 #include "../shared/common.h"
 #include <unistd.h>
@@ -29,14 +24,12 @@ bool SocketClient::connect_to_server(const QString& host, int port, const QStrin
 
     username_ = username;
 
-    // Create socket
     socket_fd_ = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_fd_ < 0) {
         emit error_occurred("Failed to create socket");
         return false;
     }
 
-    // Connect to server
     sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
@@ -49,9 +42,8 @@ bool SocketClient::connect_to_server(const QString& host, int port, const QStrin
         return false;
     }
 
-    // Send username as first message
     Message msg;
-    strncpy(msg.user, username_.toStdString().c_str(), MAX_USERNAME_LEN - 1);
+    strncpy(msg.username, username_.toStdString().c_str(), MAX_USERNAME_LEN - 1);
     strncpy(msg.timestamp, Message::get_current_timestamp().c_str(), MAX_TIMESTAMP_LEN - 1);
     strncpy(msg.text, "[JOINED]", MAX_MESSAGE_LEN - 1);
 
@@ -77,13 +69,11 @@ void SocketClient::disconnect() {
     connected_ = false;
 
     if (socket_fd_ >= 0) {
-        // Shutdown both send and receive to unblock any blocking recv() calls
         shutdown(socket_fd_, SHUT_RDWR);
         close(socket_fd_);
         socket_fd_ = -1;
     }
 
-    // Detach the thread instead of joining to avoid blocking the GUI thread
     if (receive_thread_.joinable()) {
         receive_thread_.detach();
     }
@@ -95,7 +85,7 @@ bool SocketClient::send_message(const QString& text) {
     if (!connected_) return false;
 
     Message msg;
-    strncpy(msg.user, username_.toStdString().c_str(), MAX_USERNAME_LEN - 1);
+    strncpy(msg.username, username_.toStdString().c_str(), MAX_USERNAME_LEN - 1);
     strncpy(msg.timestamp, Message::get_current_timestamp().c_str(), MAX_TIMESTAMP_LEN - 1);
     strncpy(msg.text, text.toStdString().c_str(), MAX_MESSAGE_LEN - 1);
 
@@ -106,7 +96,7 @@ void SocketClient::receive_loop() {
     Message msg;
     while (!should_stop_ && ChatUtils::recv_message(socket_fd_, msg)) {
         emit message_received(
-            QString::fromUtf8(msg.user),
+            QString::fromUtf8(msg.username),
             QString::fromUtf8(msg.timestamp),
             QString::fromUtf8(msg.text)
         );
